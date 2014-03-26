768d63028c3ff18419e3b1d66efaea2127cb1f2f
Nazar Hussain <nazarhussain@gmail.com>
2014-03-26 17:35:21 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChefTest::Application.config.secret_token = '9745a7c844e33e2d322bd292a169ffde6c40bc6842c5ac2e48c0cdf1f5b3f92e0ba9846e737c561e398e2648983bff109d85940af2af30a99c8e1d16330eb38d'
